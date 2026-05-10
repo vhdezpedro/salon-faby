54305015e6a530980dea1bb76d8b99edb2a4062e
Pedro Villanueva <vhdezpedro@gmail.com>
2026-05-09 23:16:12 -0300

    Se renderiza el componente DayView con los  appointments, el render toma en cuenta la hora de la cita y sobre esa hora pinta el appointment. De igual manera se base en la duración de la misma para modificar la altura

--- frontend/src/components/DayView.jsx
+++ frontend/src/components/DayView.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import { months } from "../utilities/months";
 import { daysOfWeek } from "../utilities/daysOfWeek";
@@ -14,12 +14,46 @@ function DayView(props) {
     setCurrentMonth,
     setCurrentYear,
   } = props;
+
+  /*   const [actualDay, setActualDay] = useState(currentDay.getDate());
+  const [touchStart, setTouchStart] = useState(null); */
   const { year, month, day } = useParams();
 
   const dayNumber = new Date(year, month, day).getDay();
 
+  /*   const handleTouchStart = (e) => {
+    setTouchStart(e.touches[0].clientX);
+  };
+
+  const prevDay = () => {
+    setCurrentMonth((prev) => (prev === 0 ? 11 : prev - 1));
+    setCurrentYear((prev) => (currentMonth === 0 ? prev - 1 : prev));
+  };
+  const nextDay = () => {
+    setCurrentMonth((prev) => (prev === 11 ? 0 : prev + 1));
+    setCurrentYear((prev) => (currentMonth === 11 ? prev + 1 : prev));
+  };
+
+  const handleTouchEnd = (e) => {
+    if (!touchStart) return;
+    const touchEnd = e.changedTouches[0].clientX;
+    const diff = touchStart - touchEnd;
+
+    if (Math.abs(diff) > 50) {
+      if (diff > 0) nextDay();
+      else prevDay();
+    }
+    setTouchStart(null);
+  }; */
+
+  console.log(appointments);
+
   return (
-    <div className="daily-view">
+    <div
+      className="daily-view"
+      /* onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd} */
+    >
       <NavBar currentMonth={month} currentYear={year} />
       <div className="day-grid">
         <div className="actual-day">
@@ -48,7 +82,36 @@ function DayView(props) {
             </>
           );
         })}
-        <div className="prueba-0">Hola</div>
+        {appointments &&
+          appointments
+            .filter((appt) => {
+              const apptDate = new Date(appt.date);
+              return (
+                apptDate.getDate() + 1 == day &&
+                apptDate.getMonth() == month &&
+                apptDate.getFullYear() == year
+              );
+            })
+            .map((appt) => {
+              const apptHour = parseInt(appt.time.split(":")[0]);
+              const apptMin = parseInt(appt.time.split(":")[1]);
+              const top = apptHour * 22 + (apptMin / 60) * 22;
+              const height = appt.service.duration * 0.333;
+
+              console.log(apptHour, apptMin, top, height);
+              return (
+                <div
+                  key={appt.id}
+                  style={{
+                    "--my-top": `${top}px`,
+                    "--my-height": `${height}px`,
+                  }}
+                  className="hour-appt"
+                >
+                  {appt.name}-{appt.service.name}
+                </div>
+              );
+            })}
       </div>
     </div>
   );
